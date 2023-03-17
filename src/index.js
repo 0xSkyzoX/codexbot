6c6ce4836bfe187b6c41e260b6c3a0ad596f52ac
SkyzoX Codes <skyzoxcodes@gmail.com>
2023-03-17 22:18:49 +0100

    setting react to oauth2 discord

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import CommamdsPage from './pages/commands';
 import ErrorPage from './pages/errorPage/error';
 import FeaturePage from './pages/features';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route exact index element={<App />} />
-      <Route path='/commands' element={<CommamdsPage />} />
-      <Route path='/commands/build'></Route>
-      <Route path='*' element={<ErrorPage />}/>
-      <Route path='/features' element={<FeaturePage />}/>
-    </Routes>
-  </BrowserRouter>
+    <Router>
+      <Route path='/'>
+        <Route exact elements={<App />} />
+
+      <Route path='/commands'  >
+        <CommamdsPage />
+      </Route>
+      <Route path='/features' >
+        <FeaturePage />
+        </Route>
+      </Route>
+      
+    </Router>
+
 );
 
