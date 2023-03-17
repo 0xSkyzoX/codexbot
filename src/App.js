f49fed8166cf3e497fa98007314cc55861a25416
SkyzoX Codes <skyzoxcodes@gmail.com>
2023-03-17 01:06:46 +0100

    new Styling Design

--- src/App.js
+++ src/App.js
@@ -17,19 +17,14 @@ function App() {
             <p className='description-index-page'>Make your coding experience with your own Community <br />server, Code Debuging, Posting Codes, Share Projects...</p>
             <button className="start-button-index" onClick={() => window.open('http://localhost:3001/auth/redirect')}>Get Started</button>   
           </div>
-
           <img src='./images/snap5.png' width='55%' height='35%' alt='' style={{ marginTop: "90px", opacity: 0.9 }} className="img-coding-index" />
-        </div>
-        
-            
+        </div>  
       </main>
       <h1 className='features-index-title' style={{textAlign: "center"}}>See our Special Features</h1>
       <hr style={{margin: "0px 7%", 
   border: "0",
   height: "3px",
-  backgroundImage: "linear-gradient(0deg, grey, transparent)",
-
-}}/>
+  backgroundImage: "linear-gradient(0deg, grey, transparent)"}}/>
       <div id="features-index-item">
         <div id='features-index-content-inside'>
           <img alt='' src='./images/sclogo.png' width='140px' id='features-index-img' />
@@ -38,12 +33,10 @@ function App() {
             <img alt='' src='./images/sclogo.png' width='140px' id='features-index-img-mobile' />
             <h1 id='features-index-title'>Special Code Support</h1>
             </div>
-          
           <div  >
           <p id='features-index-description'>CodeX by Special Code, se we support all special code features, like posting code, and projects, ext... and you can link your special code account and customize your code content... learn more about special code!</p>
           </div>
           </div>
-          
         </div>
       </div>
       <hr style={{margin: "0 40% 0px 1%",  border: "0",
@@ -51,17 +44,17 @@ function App() {
   backgroundImage: "linear-gradient(90deg, grey, transparent)",}}></hr>
       <div id="features-index-item">
         <div id='features-index-content-inside'>
-          
           <div id='features-index-tide' className='pc-tide'>
-            <div style={{display:"flex"}}>
-            
+            <div>
+              <div style={{display:"flex"}}>
                <h1 id='features-index-title' className='pc'>Coding Community</h1>
                <img alt='' src='./images/codexsupport.png' width='140px' id='features-index-img-mobile' className='cc-img' />
             </div>
-         
           <p id='features-index-description' className='pc-descrip' >CodeX Support Server, This is a Discord server helps you to communicate and talking about programming, design, ai... anything about Technology! and you can share your project there and make your experience with coding bot features!</p>
+            </div>
+            
           </div>
-          <img alt='' src='./images/codexsupport.png' width='140px' id='features-index-img' />
+          <img alt='' src='./images/codexsupport.png' width='140px' id='features-index-img' className='cxs'/>
         </div>
       </div>
       <hr style={{margin: "0 1% 0 40%",  border: "0",
@@ -75,15 +68,11 @@ function App() {
             <img alt='' src='./images/codexbotavatar.png' width='140px' id='features-index-img-mobile'/>
               <h1 id='features-index-title' >CodeX Discord Bot</h1>
             </div>
-          
           <div >
           <p id='features-index-description'>Easy to use! CodeX is linked with discord bot that's mean you can customize and use this bot whatever you want in you own server, and the best you can share your projects and posts will be shown in this website!</p>
           </div>
-          
           </div>
-          
         </div>
-        
       </div>
       <hr style={{margin: "70px 7%",  border: "0",
   height: "3px",
--- src/components/elements/featuresIntroduction.js
+++ src/components/elements/featuresIntroduction.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import "./css/features.css"
 function IntroFeature() {
   return (
     <div>
-     <h1 style={{fontFamily: 'ui-system, sans-serif',color: "whitesmoke", fontSize: "50px", margin: "80px 10px 20px 40px"}}>Start your Coding <br />Experience now <br />with our Features</h1>
-     <p style={{color: "grey", marginLeft: "55px"}}>Try our best features <a href='./features/start'>here</a>!</p>
+      <div>
+      <h1 style={{color: "whitesmoke", fontSize: "4rem", margin: "70px 10px 20px 6rem"}} className="features-page-title">Start your Coding <br />Experience with<br />our Features</h1>
+     <p style={{ marginLeft: "7rem", }} className="description-features-page">Best try with our features, learn, enjoy and code while you <br/> using our features will be the best, Try our features <a href='./features/start' style={{textDecoration: "none"}} className="a-btn-features">here</a>!</p>
+      </div>
+     
     </div>
   )
 }
