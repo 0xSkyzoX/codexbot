86c670426513e22e272d14fc008f3702058c0370
SkyzoX Codes <skyzoxcodes@gmail.com>
2023-03-19 00:12:22 +0100

    Deploying to "codexbot.xyz"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import Navbar from './components/navbar'
 import "./App.css"
 import { useTranslation } from 'react-i18next'
