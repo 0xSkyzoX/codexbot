ed303c4932abea5f4dace25ef930ac2071df10ee
SkyzoX Codes <skyzoxcodes@gmail.com>
2023-03-19 00:14:25 +0100

    Merge To Firebase Hosting

--- src/components/elements/featuresIntroduction.js
+++ src/components/elements/featuresIntroduction.js
@@ -13,7 +13,7 @@ function IntroFeature() {
       <div style={lang === "ar" ? {display: 'flex', flexDirection: "row-reverse"}:{display: "flex"}}>
          <div>
       <h1 style={lang === "ar"? {color: "whitesmoke", margin: "70px 7rem 20px 10rem", textAlign: "end"}:{color: "whitesmoke", margin: "70px 10px 20px 6rem"}} className="features-page-title">{title[0]}<br />{title[1]}<br />{title[2]}</h1>
-     <p style={lang === "ar" ? {textAlign: "end", marginRight: "6rem"}:{ marginLeft: "7rem", }} className="description-features-page">{description[0]}<br/>{description[1]}<a href='/features/start' style={{textDecoration: "none"}} className="a-btn-features"></a></p>
+     <p style={lang === "ar" ? {textAlign: "end", marginRight: "6rem"}:{ marginLeft: "7rem", }} className="description-features-page">{description[0]}<br/>{description[1]}<a href='/features/start' style={{textDecoration: "none"}} className="a-btn-features">Here!</a></p>
       </div>
      <SetInfoData />
       </div>
